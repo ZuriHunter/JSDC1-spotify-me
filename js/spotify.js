0b60618da651dcd00663f5b42e7a4c099484bed2
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-04-20 18:41:50 -0400

    first commit

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,45 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+var $userInput = $("#search-keyword");
+var $form = $("#search");
+var $results = $('#results');
+// var $submitBtn = $("#submit-button");
 
+$form.on('submit', function(event){
+  event.preventDefault();
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+  var theInput = $userInput.val();
+  var searchType = $('#search-type').val();
+  $.ajax ({
+      url: 'https://api.spotify.com/v1/search?q=' + theInput + '&type=' + searchType,
 
+      success: function(response) {
+        console.log(response)
+        console.log(response.items)
+        
+        plural = searchType + 's';
+        console.log(plural)
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+        response[plural].items.forEach( function(item){
+          listItem = $("<li>"+ item.name  + "</li>");
+          console.log(listItem);
+
+          $results.append(listItem);
+          console.log($results);
+
+        })
+
+      }
+  })
+});
+
+//
+// function searchByArtist(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+//
+// }
+//
+//
+// function searchByTrack(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+// }
